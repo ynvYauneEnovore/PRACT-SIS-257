22eaf141d5147f026dc7a22b1304b33f598edab6
ynvYauneEnovore <yovanuxf@gmail.com>
2023-03-21 10:02:38 -0400

    nueva pruebas probando tipos de datos

--- hello.cpp
+++ hello.cpp
@@ -10,33 +10,53 @@ public:
     Empleado() {}
 
     // Constructor que inicializa el nombre y el número de empleado
-    Empleado(string nombre, int numerodeempleado) {
+    Empleado(string nombre, string apellidos, int numerodeempleado, string email, float sueldo, bool experto) {
         this->nombre = nombre;
+        this->apellidos = apellidos;
         this->numerodeempleado = numerodeempleado;
+        this->email = email;
+        this->sueldo = sueldo;
+        this->experto = experto;
         }
 
     // Función para leer los datos del teclado
     void LeerDatos() {
-        cout << "Ingrese el nombre del Empleado:";
+        cout << "Ingrese nombre(s) del Empleado(a):";
         getline(cin, nombre);
-        cout << "Ingrese el numero de Empleado:";
+        cout << "Ingrese apellido(s) del Empleado(a):";
+        getline(cin, apellidos);
+        cout << "Ingrese el ID de Empleado:";
         cin >> numerodeempleado;
+         cout << "Ingrese el correo de Empleado(a):";
+        cin >> email;
+         cout << "Ingrese el sueldo de Empleado(a):";
+        cin >> sueldo;
+         cout << "es experto(a)? (si=1)/(no=0):";
+        cin >> experto;
         cin.ignore(); // Ignorar el salto de línea después de leer el número de empleado
     }
 
     // Función para visualizar los datos en pantalla
     void VerDatos() {
-        cout << "Nombre: " << nombre << endl;
-        cout << "Numero de Empleado: " << numerodeempleado << endl;
+        cout << "Nombre(s): " << nombre << endl;
+        cout << "Apellidos(s): " << apellidos << endl;
+        cout << "ID empleado(a): " << numerodeempleado << endl;
+        cout << "Correo empleado(a): " << email << endl;
+        cout << "Sueldo empleado(a): " << sueldo << endl;
+        cout << "Es experto? -> (si=1)/(no=0): " << experto << endl;
     }
 
 private:
     string nombre;
-    string numerodeempleado;
+    int numerodeempleado;
+    string apellidos;
+    string email;
+    float sueldo;
+    bool experto;
 };
 
 int main() {
-    const int numEmpleados = 50;
+    const int numEmpleados = 3;
     Empleado empleados[numEmpleados];
 
     // Llenar el array con los datos de los empleados
